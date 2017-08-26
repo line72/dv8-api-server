6d324e1a8e1fab3c90891bfdea3257fe61340b64
Marcus Dillavou <line72@line72.net>
2017-08-26 10:17:12 -0500

    Fixed the location of the db and log

--- src/dependencies.php
+++ src/dependencies.php
@@ -14,7 +14,7 @@ $container['logger'] = function ($c) {
 
 // set up our database
 $container['db'] = function($c) {
-    $pdo = new PDO("sqlite:dv8.db");
+    $pdo = new PDO("sqlite:../../dv8.db");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
