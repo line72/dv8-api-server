3b5c32e49901d821da14565107fa89eb35e724cc
Marcus Dillavou <line72@line72.net>
2017-09-18 08:56:58 -0500

    Basic CORS support

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,19 @@
 <?php
+
+// Add in CORS Support
+// https://www.slimframework.com/docs/cookbook/enable-cors.html
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 // Routes
 
 $app->get('/routes', function ($request, $response, $args) {
